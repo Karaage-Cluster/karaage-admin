64ecc110418ac681a43156b1fb4618e1ef0769d8
Brian May <brian@microcomaustralia.com.au>
2014-02-12 10:46:09 +1100

    Updates for latest karaage3 in git.

--- kgadmin/conf/settings.py
+++ kgadmin/conf/settings.py
@@ -1,12 +1,6 @@
 # Packaged defined Karaage-admin settings
 from karaage.conf.settings import *
 
-AJAX_LOOKUP_CHANNELS = {
-    'person' : ( 'karaage.people.lookups', 'PersonLookup'),
-    'group' : ( 'karaage.people.lookups', 'GroupLookup'),
-    'project' : ( 'karaage.projects.lookups', 'ProjectLookup'),
-}
-
 TEMPLATE_DIRS += (
     "/usr/share/kgadmin/templates",
 )
@@ -18,10 +12,8 @@ ROOT_URLCONF = 'kgadmin.conf.urls'
 STATIC_ROOT = '/var/lib/karaage-admin/static'
 STATIC_URL = '/kgadmin_media/'
 
-LOGIN_URL="/kgadmin/profile/login/"
-
 TEMPLATE_CONTEXT_PROCESSORS += ('karaage.common.context_processors.admin',)
 
-USAGE_IS_PUBLIC = True
+ADMIN_REQUIRED = True
 
 execfile("/etc/karaage/admin_settings.py")

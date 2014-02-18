4b057e566a54fef38e4e451f80c9a698a5dcf215
Brian May <brian@microcomaustralia.com.au>
2014-02-19 10:25:52 +1100

    Remove template now in Karaage.

--- kgadmin/conf/settings.py
+++ kgadmin/conf/settings.py
@@ -1,18 +1,12 @@
 # Packaged defined Karaage-admin settings
 from karaage.conf.settings import *
 
-TEMPLATE_DIRS += (
-    "/usr/share/kgadmin/templates",
-)
-
 SITE_ID = 1
 
 ROOT_URLCONF = 'kgadmin.conf.urls'
 
 STATIC_ROOT = '/var/lib/karaage-admin/static'
 STATIC_URL = '/kgadmin_media/'
-
-TEMPLATE_CONTEXT_PROCESSORS += ('karaage.common.context_processors.admin',)
 
 ADMIN_REQUIRED = True
 
